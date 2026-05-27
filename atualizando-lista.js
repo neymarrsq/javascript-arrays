88eefcaf0b501790227998b3aa99f9269fff1bea
Neyma <neymar.queiroz@escola.pr.gov.br>
2026-05-27 15:14:19 +0000

    .

---
+++ atualizando-lista.js
@@ -0,0 +1,6 @@
+const nomes = ["João", "Ana", "Lara", "Marjorie", "Leo"]
+
+nomes.splice(1, 2);
+nomes.push("Rodrigo");
+
+console.log(nomes);
---
+++ dividindo-sala.js
@@ -0,0 +1,7 @@
+const alunos = ["João", "Vinicius", "Carla", "Caio", "Lara", "Marjorie", "Guilherme", "Aline", "Fabiana", "Andre", "Juliana", "Neymar", "Bia", "Vivian", "Isabela", "Ana", "Renon", "Renato", "Daisy", "Camilo"];
+
+const sala1 = alunos.slice(0, 10);
+const sala2 = alunos.slice(10);
+
+console.log(sala1);
+console.log(sala2);
