db93f796ede2164c67a3034bfa71717bc42f28d2
Neyma <neymar.queiroz@escola.pr.gov.br>
2026-08-06 15:21:01 +0000

    .

--- media-geral.js
+++ media-geral.js
@@ -3,10 +3,11 @@ const salaJava = [6, 5, 8, 9, 5, 6];
 const salaPython = [7, 3.5, 8, 9.5];
 
 function calculaMedia(notasDaSala) {
-    const somaDasNotas = notasDaSala.reduce((acumulador, nota) =>{
-     return acumulador + nota;
-    }, 0);
-    console.log(somaDasNotas);
-}
+    const somaDasNotas = notasDaSala.reduce((acc, nota) => acc + nota, 0);
+     const media = somaDasNotas / notasDaSala.length;
+     return media;
+    }
 
-calculaMedia(salaJS);
+    console.log(`A média da sala de JavaScript é ${calculaMedia(salaJS)}`);
+    console.log(`A media da sala em Java é ${calculaMedia(salaJava)}`);
+    console.log(`A media da sala em Python é ${calculaMedia(salaPython)}`);
