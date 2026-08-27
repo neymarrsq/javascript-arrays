fa2bc2415e4083e3158b098c921f5d95f668b1be
Neyma <neymar.queiroz@escola.pr.gov.br>
2026-08-27 15:20:46 +0000

    .

---
+++ alinhados.js
@@ -0,0 +1,10 @@
+const cliente = {
+};
+
+cliente.endereco = {
+    rua: "R. Joseph Clomber",
+    numero: 1337,
+    apartamento: true,
+    complemento: "ap 934",
+};
+console.log(cliente);
---
+++ tipos.js
@@ -0,0 +1,8 @@
+const cliente = {
+    nome: "João",
+    idade: 24,
+    email: "joao@firma.com",
+    telefone: ["1155555550", "11444444440"],
+};
+
+console.log(cliente.telefone);
